2416eb323347bcd18dfca5a462d0029b26d410db
aditya9706 <41884965+aditya9706@users.noreply.github.com>
2022-02-07 15:23:05 +0530

    Add files via upload

---
+++ 0702/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['freememory',['FreeMemory',['../classTrmm.html#a3e3916b3d67a211f8ab56e8a2c2a4731',1,'Trmm']]]
+];
---
+++ 0702/search/functions_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['trmm',['Trmm',['../classTrmm.html#a7ee8bbe6a2c10baf2bffffa15f6591e9',1,'Trmm']]],
+  ['trmmapicall',['TrmmApiCall',['../classTrmm.html#a7eca9581b49c543034d23f2b99b5be4e',1,'Trmm']]]
+];
